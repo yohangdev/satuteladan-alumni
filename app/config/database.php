c2c46c10227387aec54ef4bbb08941c0a287eeb2
evan <evan@evan-VirtualBox.(none)>
2013-08-06 08:22:18 +0800

    changeback database config

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'satuteladan_devel',
 			'username'  => 'root',
-			'password'  => 'abc123',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => 'satuteladan_',
